b5198fd587c0e583dce6d288d359411a0051342c
Matthew Smith <azamatsmith@gmail.com>
2017-10-23 12:20:05 -0600

    Allow CORS

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express()
 
 const port = process.env.PORT || 3005;
 
+// ALLOW CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', (req, res) => {
   getBooks(result => res.json(result));
 })
