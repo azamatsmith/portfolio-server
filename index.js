2759d863cec4f5bcb1b4681682d084f5bda07497
Matthew Smith <azamatsmith@gmail.com>
2017-10-23 11:54:53 -0600

    Update port to use process.env

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const getBooks = require('./Books');
 const app = express()
 
+const port = process.env.PORT || 3005;
+
 app.get('/', (req, res) => {
   getBooks(result => res.json(result));
 })
 
-app.listen(3000, function () {
-  console.log('tw-portfolio app listening on port 3000')
+app.listen(port, () => {
+  console.log('tw-portfolio app listening on port 3005')
 })
 
